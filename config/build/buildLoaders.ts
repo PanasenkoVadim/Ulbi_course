68afe6aa9f253f9e388b000aba274bb190fc5cf3
Vadim Panasenko <panasenko@e-krit.ru>
2024-02-06 23:52:40 +0300

    add loaders

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,33 +1,47 @@
-import { BuildOptions } from "./types/config";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import webpack from "webpack";
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import webpack from 'webpack'
+import { BuildOptions } from './types/config'
 
 export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
-  const cssLoader = {
-    test: /\.(sc|c)ss$/i,
-    use: [
-      options.isDev ? "style-loader" : MiniCssExtractPlugin.loader,
-      {
-        loader: "css-loader",
-        options: {
-          modules: {
-            auto: (resPath: string) => Boolean(resPath.includes(".module.")),
-            localIdentName: options.isDev
-              ? "[path][name]__[local]"
-              : "[local]_[hash:base64:8]",
-          },
-        },
-      },
+	const svgLoader = {
+		test: /\.svg$/,
+		use: ['@svgr/webpack'],
+	}
 
-      "sass-loader",
-    ],
-  };
+	const fileLoader = {
+		test: /\.(png|jpe?g|gif)$/i,
+		use: [
+			{
+				loader: 'file-loader',
+			},
+		],
+	}
 
-  const typescriptLoader = {
-    test: /\.tsx?$/,
-    use: "ts-loader",
-    exclude: /node_modules/,
-  };
+	const cssLoader = {
+		test: /\.(sc|c)ss$/i,
+		use: [
+			options.isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+			{
+				loader: 'css-loader',
+				options: {
+					modules: {
+						auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+						localIdentName: options.isDev
+							? '[path][name]__[local]'
+							: '[local]_[hash:base64:8]',
+					},
+				},
+			},
 
-  return [typescriptLoader, cssLoader];
+			'sass-loader',
+		],
+	}
+
+	const typescriptLoader = {
+		test: /\.tsx?$/,
+		use: 'ts-loader',
+		exclude: /node_modules/,
+	}
+
+	return [typescriptLoader, cssLoader, svgLoader, fileLoader]
 }
--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -1,19 +1,20 @@
-import { FC } from "react";
-import Container from "../../Container/Container";
-import css from "./Header.module.scss";
-import { Navbar } from "widgets/Navbar";
-import { ThemeSwitcher } from "shared/ui/ThemeSwitcher/";
-
+import { FC } from 'react'
+import Icon from 'shared/static/images/test.svg'
+import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher/'
+import { Navbar } from 'widgets/Navbar'
+import Container from '../../Container/Container'
+import css from './Header.module.scss'
 export const Header: FC = () => {
-  return (
-    <header className={css.header}>
-      <Container>
-        <div className={css.inner}>
-          <div className={css.logo}>LOGO</div>
-          <ThemeSwitcher />
-          <Navbar />
-        </div>
-      </Container>
-    </header>
-  );
-};
+	return (
+		<header className={css.header}>
+			<Container>
+				<div className={css.inner}>
+					<div className={css.logo}>LOGO</div>
+					<ThemeSwitcher />
+					<Icon />
+					<Navbar />
+				</div>
+			</Container>
+		</header>
+	)
+}
