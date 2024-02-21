da4cf129cfab26674e4e18dd0480a503f12b7f87
Vadim Panasenko <panasenko@e-krit.ru>
2024-02-21 20:48:04 +0300

    test

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -2,7 +2,7 @@ import { useTranslation } from 'react-i18next'
 
 const AboutPage = () => {
   const { t } = useTranslation('about')
-  return <div>{t('О нас')}</div>
+  return <div><h1>{t('О нас')}</h1><ul><li>123123</li></ul></div>
 }
 
 export default AboutPage
