dbb614d61489451cf1913c3d6ad0801c5f43abb0
Vadim Panasenko <panasenko@e-krit.ru>
2024-02-25 14:34:58 +0300

    install json-server; fix theme

--- src/widgets/Switchers/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/Switchers/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -3,28 +3,29 @@ import { type FC } from 'react'
 import { useTranslation } from 'react-i18next'
 import classNames from 'shared/lib/classNames/classNames'
 import css from './ThemeSwitcher.module.scss'
+import { Theme } from 'app/providers/ThemeProvider/lib/ThemeContext'
 
 interface ThemeSwitcherProps {
-  className?: string
+	className?: string
 }
 
 export const ThemeSwitcher: FC = ({ className }: ThemeSwitcherProps) => {
-  const { theme, toggleTheme } = useTheme()
-  const { t } = useTranslation()
-  return (
-        <button
-          onClick={toggleTheme}
-          className={classNames(css.btn, {}, [className])}
-        >
-          <span>{t('Светлая')}</span>
-          <span
-                className={classNames(
-				  css.round,
-				  { [css.switched]: theme === 'dark' },
-				  []
-                )}
-            ></span>
-          <span>{t('Тёмная')}</span>
-      </button>
-  )
+	const { theme, toggleTheme } = useTheme()
+	const { t } = useTranslation()
+	return (
+		<button
+			onClick={toggleTheme}
+			className={classNames(css.btn, {}, [className])}
+		>
+			<span>{t('Светлая')}</span>
+			<span
+				className={classNames(
+					css.round,
+					{ [css.switched]: theme === Theme.DARK },
+					[]
+				)}
+			></span>
+			<span>{t('Тёмная')}</span>
+		</button>
+	)
 }
